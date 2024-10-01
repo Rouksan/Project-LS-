d32bddf915efdd1722ef6268bc375fcf003d2fc4
loubna kherdali <loubna.kherdali@umail.uom.ac.mu>
2024-10-01 12:28:42 +0400

    Added the function prototypes to users.h

--- src/users.h
+++ src/users.h
@@ -4,3 +4,14 @@
          
               users.h file
 */
+#ifndef USERS_H
+#define USERS_H
+
+#include <stdio.h>
+
+// Function prototypes for User Management
+void addUser();
+void removeUser();
+void viewAllUsers();
+
+#endif // USERS_H
