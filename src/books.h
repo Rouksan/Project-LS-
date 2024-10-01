49aac3bb269c124c2b27ae6167d56f327f5bd2ec
Rouksan Kherdali <rouksan.kherdali@umail.uom.ac.mu>
2024-10-01 11:58:03 +0400

    Added the function prototypes to books.h

--- src/books.h
+++ src/books.h
@@ -4,3 +4,17 @@
          
               books.h file
 */
+#ifndef BOOKS_H
+#define BOOKS_H
+
+#include <stdio.h>
+
+// Function prototypes for Book Management
+void addBook();
+void removeBook();
+void viewAllBooks();
+void searchBooks();
+void viewAvailableBooks();
+
+
+#endif // BOOKS_H
