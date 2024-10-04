ad039a83ef42faa37a6b6c040a2ad6e01f24516f
Kaushik Ramkorun <kaushik.ramkorun@umail.uom.ac.mu>
2024-10-04 10:56:10 +0400

    Added the function prototype

--- src/transactions.h
+++ src/transactions.h
@@ -4,3 +4,16 @@
          
               transactions.h file
 */ 
+
+#ifndef TRANSACTIONS_H
+#define TRANSACTIONS_H
+
+#include <stdio.h>
+
+// Function prototypes for Transaction Management
+void issueBook();
+void returnBook();
+void viewAllTransactions();
+void viewUserTransactions();
+
+#endif // TRANSACTIONS_H
