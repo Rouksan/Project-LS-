512111123cad6dc65648cdf88f5577cb841f9882
Kaushik Ramkorun <kaushik.ramkorun@umail.uom.ac.mu>
2024-10-04 10:51:40 +0400

    added descriptions to transactions.c, transactions.h  and  menu.h

---
+++ src/menu.h
@@ -0,0 +1,6 @@
+/*
+             @author : Kaushik
+             Date    : 01.10.2024
+         
+              menu.h file
+*/ 
---
+++ src/transactions.c
@@ -0,0 +1,6 @@
+/*
+             @author : Kaushik
+             Date    : 01.10.2024
+         
+              transactions.c file
+*/ 
