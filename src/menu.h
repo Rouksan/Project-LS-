4b201f17a58ab4e34071381f2385f89d33ae6c6e
Kaushik Ramkorun <kaushik.ramkorun@umail.uom.ac.mu>
2024-10-04 11:01:07 +0400

    Added the function prototypes

--- src/menu.h
+++ src/menu.h
@@ -4,3 +4,16 @@
          
               menu.h file
 */ 
+
+#ifndef MENU_H
+#define MENU_H
+
+#include <stdio.h>
+
+// Function prototypes for Menus
+void showMainMenu();
+void showAdminMenu();
+void showLibrarianMenu();
+void showUserMenu();
+
+#endif // MENU_H
